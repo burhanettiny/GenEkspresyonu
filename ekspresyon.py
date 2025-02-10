788833fe0339ef6e8e2f3b86bffeabde6c5e7e40
burhanettiny <mailtoburhanettin@gmail.com>
2025-02-10 17:01:59 +0300

    Update ekspresyon.py

--- ekspresyon.py
+++ ekspresyon.py
@@ -175,8 +175,4 @@ if stats_data:
         y=[average_sample_delta_ct, average_sample_delta_ct],  # Y ekseninde ortalama değer
         mode='lines',
         line=dict(color='black', dash='dot', width=4),  # Kesik siyah çizgi
-        name='Hasta Grubu Ortalama'
-    ))
-
-    # Grafiği göster
-    st.plotly_chart(fig)
+        name='Hasta Grubu
