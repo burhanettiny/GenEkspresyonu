0777647cdaf47e2ccccf936e201ca2c96c5124a8
burhanettiny <mailtoburhanettin@gmail.com>
2025-02-12 16:48:58 +0300

    Update ekspresyon.py

--- ekspresyon.py
+++ ekspresyon.py
@@ -288,7 +288,8 @@ def create_pdf(results, stats, input_df):
     
     explanation = (
         "İstatistiksel değerlendirme sürecinde veri dağılımı Shapiro-Wilk testi ile analiz edilmiştir. "
-        "Normallik sağlanırsa, Gruplar arasındaki varyans eşitliği Levene testi ile varyans eşitliği kontrol edilmiştir. "
+        "Normallik sağlanırsa,"
+        "Gruplar arasındaki varyans eşitliği Levene testi ile varyans eşitliği kontrol edilmiştir. "
         "Varyans eşitliği varsa bağımsız örneklem t-testi, yoksa Welch t-testi uygulanmıştır. "
         "Eğer normal dağılım sağlanmazsa, Parametrik olmayan Mann-Whitney U testi kullanılmıştır. "
         "p-değerinin 0.05 eşik değerinden küçük olup olmadığına göre belirlenmiştir. "
