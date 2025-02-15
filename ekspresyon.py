38ad28ff930f2deb4adc76033561e71261e518c3
burhanettiny <mailtoburhanettin@gmail.com>
2025-02-15 12:02:18 +0300

    Update ekspresyon.py

--- ekspresyon.py
+++ ekspresyon.py
@@ -340,7 +340,7 @@ def create_pdf(results, stats, input_df):
     buffer.seek(0)
     return buffer
 
-if st.button("📥 PDF Raporu İndir"):
+if st.button("📥 PDF Raporu Hazırla"):
     if input_values_table:
         pdf_buffer = create_pdf(data, stats_data, pd.DataFrame(input_values_table))
         st.download_button(label="PDF Olarak İndir", data=pdf_buffer, file_name="gen_ekspresyon_raporu.pdf", mime="application/pdf")
