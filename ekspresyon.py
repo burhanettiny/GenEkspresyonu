9e3b4f946fc7afbcdb9c14b747f4c82a4988fb5e
burhanettiny <mailtoburhanettin@gmail.com>
2025-02-21 16:56:46 +0300

    Update ekspresyon.py

--- ekspresyon.py
+++ ekspresyon.py
@@ -175,8 +175,8 @@ for i in range(num_target_genes):
     
     # Hasta Grubu Verileri
     for j in range(num_patient_groups):
-        salha = translations[language_code]["salha"]
-        st.subheader(f"{salha} {i+1}")
+        salha = translations[language_code]["hast"]
+        st.subheader(f"{hast} {i+1}")
         
         sample_target_ct_text = translations[language_code]["hast"].format(i=i+1)
         sample_target_ct = st.text_area(sample_target_ct_text, key=f"sample_target_ct_{i}")
