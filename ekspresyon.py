e164a2fec87d76dc2593167e75ef7becaddc8e0b
burhanettiny <mailtoburhanettin@gmail.com>
2025-02-21 14:47:14 +0300

    Update ekspresyon.py

--- ekspresyon.py
+++ ekspresyon.py
@@ -135,7 +135,7 @@ for i in range(num_target_genes):
     control_target_ct_text = translations[language_code]["ctrl_trgt_ct"].format(i=i)
     control_target_ct = st.text_area(control_target_ct_text, key=f"control_target_ct_{i}")
     
-    control_reference_ct_text = translations[language_code]["ctrl_ref_ct"].format(i+1)
+    control_reference_ct_text = translations[language_code]["ctrl_ref_ct"].format(i=i)
     control_target_ct = st.text_area(control_reference_ct_text, key=f"control_reference_ct_{i}")
    
     control_target_ct_values = parse_input_data(control_target_ct)
