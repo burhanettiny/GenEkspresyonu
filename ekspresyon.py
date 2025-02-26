a8f4bec43d461368920fbc4d1bee0f9cb7054d3d
burhanettiny <mailtoburhanettin@gmail.com>
2025-02-26 12:18:16 +0300

    Update ekspresyon.py

--- ekspresyon.py
+++ ekspresyon.py
@@ -231,11 +231,11 @@ for i in range(num_target_genes):
     for idx in range(min_control_len):
         input_values_table.append({
             translations[language_code]["sample_number"]: sample_counter,
-            translations[language_code]["hfg"]: f"{translations[language_code]['hfg']} {j+1}",
-            translations[language_code]["group"]: f"{translations[language_code]['hast']} {j+1}",
+            translations[language_code]["hfg"]: f"{translations[language_code]['hfg']} {i+1}",
+            translations[language_code]["group"]: f"{translations[language_code]['salha']} {i+1}",
             translations[language_code]["gene_ct_value"]: sample_target_ct_values[idx],
             translations[language_code]["reference_ct"]: sample_reference_ct_values[idx],
-            translations[language_code]["delta_cth"]: sample_delta_ct[idx]  # Hasta için ΔCt (Hasta)
+            translations[language_code]["delta_ct"]: sample_delta_ct[idx]  
         })
         sample_counter += 1
 
@@ -273,10 +273,10 @@ for i in range(num_target_genes):
             input_values_table.append({
                 translations[language_code]["sample_number"]: sample_counter,
                 translations[language_code]["hfg"]: f"{translations[language_code]['hfg']} {i+1}",
-                translations[language_code]["group"]: f"{translations[language_code]['salha']} {i+1}",
+                translations[language_code]["group"]: f"{translations[language_code]['hast']} {j+1}",
                 translations[language_code]["gene_ct_value"]: control_target_ct_values[idx],
                 translations[language_code]["reference_ct"]: control_reference_ct_values[idx], 
-                translations[language_code]["delta_ct"]: control_delta_ct[idx]  # Kontrol için ΔCt
+                translations[language_code]["delta_cth"]: control_delta_ct[idx]  
             })
             sample_counter += 1
         
